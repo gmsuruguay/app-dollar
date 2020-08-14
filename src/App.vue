bcc5ac8d53fec182bca09f273fe8b117076e22a6
martin_dev <gmsuruguay@gmail.com>
2020-08-14 19:02:23 -0300

    se creo metodo para llamar a la api

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <v-app>
-   
-
     <v-main>
-      <HelloWorld/>
+      <v-container>
+        <router-view/>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+   
   },
 
   data: () => ({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-row>
+    <v-col xs="12">
+      <v-card>
+        <v-date-picker 
+        full-width
+        locale="es-AR"
+        :min = "minimo"
+        :max = "maximo"
+        @change="getDolar(fecha)"
+        v-model="fecha"></v-date-picker>
+      </v-card>
+      <v-card color="#26c6da" dark>
+        <v-card-text class="display-1 text-center">
+           {{valor}}
+        </v-card-text>
+      </v-card>
+      
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      fecha : new Date().toISOString().substring(0,10),
+      minimo : '2000',
+      maximo : new Date().toISOString().substring(0,10),
+      valor : null
+    }
+  },
+  methods:{
+    async getDolar(dia){
+      let arrayFecha = dia.split("-")
+      let newFecha = arrayFecha[2]+'-'+arrayFecha[1]+'-'+arrayFecha[0]
+      try {        
+        let datos = await axios.get(`https://mindicador.cl/api/dolar/${newFecha}`)
+        
+        if (datos.data.serie.length > 0) {
+          this.valor = await datos.data.serie[0].valor
+          
+        } else {
+          this.valor = "Sin resultado"
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  },
+  created(){    
+    this.getDolar(this.fecha)
   }
 }
 </script>
